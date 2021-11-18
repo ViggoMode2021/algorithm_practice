58c3550da9589966139b6e294299cb603c8ff75d
ViggoMode210 <86791607+ViggoMode2021@users.noreply.github.com>
2021-11-18 18:28:51 -0500

    Update quick_sort.py
    Adding hashing # notes. Wrapping the brain around it.

--- quick_sort.py
+++ quick_sort.py
@@ -1,19 +1,22 @@
-def quick_sort(sequence):
+def quick_sort(sequence): #takes in a sequence of unsorted values
     length = len(sequence)
     if length <= 1:
-        return sequence
+        return sequence #allows algorithm to skip over sequences that have a length of one or zero
     else:
-        pivot = sequence.pop()
+        pivot = sequence.pop()  #remove last element of sequence and return it
 
-    items_greater = []
-    items_lower = []
+    items_greater = [] # items greater than pivot point
+    items_lower = [] # items lower than pivot point
 
-    for item in sequence:
+    for item in sequence: #compare each item left in sequence to pivot point
         if item > pivot:
             items_greater.append(item)
+
         else:
             items_lower.append(item)
 
-    return quick_sort(items_lower) + [pivot] + quick_sort(items_greater)
+    return quick_sort(items_lower) + [pivot] + quick_sort(items_greater) #apply algorithm to sublists and concatonation of pivot point
 
-print(quick_sort([6, 2, 1, 4, 5, 6, 8, 9]))
+
+print(quick_sort([1991, 1981, 1999, 2001, 2004, 2002, 1987, 1977, 1950, 1955, 1954, 1978]))
+
