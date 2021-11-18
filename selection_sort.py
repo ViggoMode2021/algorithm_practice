7a438411d0ba5b790a85c573f94addef424b6e54
ViggoMode210 <86791607+ViggoMode2021@users.noreply.github.com>
2021-11-18 16:33:38 -0500

    Create selection_sort.py

---
+++ selection_sort.py
@@ -0,0 +1,16 @@
+def selection_sort(list_a):
+    indexing_length = range(0, len(list_a)-1)
+
+    for i in indexing_length:
+        min_value = i
+
+        for j in range(i+1, len(list_a)):
+            if list_a[j] < list_a[min_value]:
+                min_value = j
+
+        if min_value != i:
+            list_a[min_value], list_a[i] = list_a[i], list_a[min_value]
+
+    return list_a
+
+print(selection_sort([7,4,1,2,5,6,7,2,57,8,9]))
